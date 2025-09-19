e2a7d5dfa9d1cf68e25020292cf9d58c171addd8
Your Name <you@example.com>
2025-09-19 15:14:40 +0300

    aded get data function

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,28 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
+  const btn = document.getElementById('addBtn');
+  function getData(){
+    
+    const toDoInput = document.getElementById('toDo');
+    const list = document.getElementById('list');
+    if(!toDoInput){
+      list.textContent = 'no tasks or error'
+     
 
+    }else{
+      const data = toDoInput.value;
+      list.append(data);
+    };
+
+
+  }
+  btn.addEventListener('click', getData);
   return (
     <>
       <h1>Todo List</h1>
       <div>
-        <input placeholder='What needs to be done?' type="text" />
+        <input id='toDo' placeholder='What needs to be done?' type="text" />
         <button id='addBtn'>Add</button>
       </div>
       <ul>
@@ -17,6 +32,7 @@ function App() {
 
     </>
   )
+  
 }
 
 export default App
